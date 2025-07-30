3d53a8b7a8c6e9e8c8b7db4dcccaa7d90782dd13
Jakub Motyl <motyl.kubo@gmail.com>
2025-07-30 18:11:23 -0400

    add powershell build utils

---
+++ BuildUploadServer.ps1
@@ -0,0 +1,141 @@
+# edit these values
+$github_pat = '<GITHUB_PERSONAL_ACCESS_TOKEN>'
+$github_username = '<GITHUB_USERNAME>'
+
+$ue_image_tag='dev-5.5.4'
+$server_config='Development'
+$project_file_name='LyraStarterGame'
+
+$registry = 'registry.edgegap.com'
+$project = '<REGISTRY_PROJECT>'
+$username = 'robot$<REGISTRY_ORGANIZATION_ID>+client-push'
+$token = '<REGISTRY_TOKEN>'
+
+# leave the rest of the script unchanged
+$buildUtilsPath = Get-Location
+Set-Location -Path $(Split-Path $PSScriptRoot -Parent)
+
+$dockerVersion = docker --version 2>&1
+
+if ($dockerVersion -match 'not recognized') {
+    Write-Error "Docker not installed, visit https://www.docker.com"
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+$dockerContainers = docker ps 2>&1
+
+if ($dockerContainers -match 'error during connect') {
+    Write-Error "Docker is not running. Please start Docker and try again."
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+$metadataFilePath = "$buildUtilsPath\package.json"
+
+if (-not (Test-Path -Path $metadataFilePath)) {
+    Write-Error "Couldn't find package.json, re-download latest version from https://github.com/edgegap/edgegap-unreal-buildutils"
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+try {
+    $localVersion = (Get-Content -Path $metadataFilePath -Raw | ConvertFrom-Json).version
+
+    # Access and print the 'version' property
+    if (-not $localVersion) {
+        throw 'Version not found.'
+    }
+} catch {
+    Write-Error "Error reading or parsing local package.json: $_"
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+try {
+    $response = Invoke-RestMethod `
+        -Uri 'https://api.github.com/repos/edgegap/edgegap-unreal-buildutils/releases/latest' `
+        -Headers @{
+            'Accept'              = 'application/vnd.github+json'
+            'X-GitHub-Api-Version'= '2022-11-28'
+            'User-Agent'          = 'Powershell'
+        } `
+        -TimeoutSec 30
+
+    $latestVersion = $response.name
+
+    if (-not $latestVersion) {
+        throw 'Version not found.'
+    }
+} catch {
+    Write-Error "Error fetching release information: $_"
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+if ([version]$localVersion -lt [version]$latestVersion) {
+    Write-Warning "Update now - new buildutils version is available at https://github.com/edgegap/edgegap-unreal-buildutils"
+}
+
+$loginResult = echo "$github_pat" | docker login ghcr.io -u "$github_username" --password-stdin 2>&1
+
+if ($loginResult -match 'unauthorized') {
+    Write-Error "Docker GHCR login failed: unauthorized. Please verify your PAT."
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Docker GHCR login failed: $login_output"
+    Read-Host -Prompt "Press Enter to exit"
+    exit $LASTEXITCODE
+}
+
+Write-Host "Docker GHCR login succeeded!"
+
+docker pull "ghcr.io/epicgames/unreal-engine:$ue_image_tag"
+
+$image = (Split-Path -Leaf $(Get-Location)).ToLower() -replace '\s+','-' -replace '[^a-z0-9-]',''
+$tag = Get-Date -UFormat "%y.%m.%d-%H.%M.%S%Z"
+
+$loginResult = echo "$token" | docker login $registry -u "$username" --password-stdin 2>&1
+
+if ($loginResult -match 'unauthorized') {
+    Write-Error "Docker login failed: unauthorized. Please verify your docker credentials."
+    Read-Host -Prompt "Press Enter to exit"
+    exit 1
+}
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Docker login failed: $login_output"
+    Read-Host -Prompt "Press Enter to exit"
+    exit $LASTEXITCODE
+}
+
+Write-Host "Docker $registry login succeeded!"
+
+docker build . `
+    -f "${buildUtilsPath}\Dockerfile" `
+    -t "${registry}/${project}/${image}:${tag}" `
+    --build-arg BUILDUTILS_FOLDER=$(Split-Path -Leaf $buildUtilsPath) `
+    --build-arg UE_IMAGE_TAG=$ue_image_tag `
+    --build-arg SERVER_CONFIG=$server_config `
+    --build-arg PROJECT_FILE_NAME=$project_file_name
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Docker build failed."
+    Read-Host -Prompt "Press Enter to exit"
+    exit $LASTEXITCODE
+}
+
+docker push "${registry}/${project}/${image}:${tag}"
+
+if ($LASTEXITCODE -ne 0) {
+    Write-Error "Docker push failed."
+    Read-Host -Prompt "Press Enter to exit"
+    exit $LASTEXITCODE
+}
+
+Start-Process "https://app.edgegap.com/application-management/applications/${image}/versions/create?name=${tag}&imageRepo=${project}/${image}&dockerTag=${tag}&vCPU=1&memory=1&utm_source=ue_buildutils&utm_medium=servers_quickstart_script&utm_content=create_version_link"
+
+Read-Host -Prompt "Press Enter to exit"
---
+++ Dockerfile
@@ -0,0 +1,47 @@
+ARG UE_IMAGE_TAG=dev-5.5.4
+# First image is used to build your project inside a container
+FROM ghcr.io/epicgames/unreal-engine:${UE_IMAGE_TAG} AS builder
+
+# Expose UDP port (still need to publish!)
+EXPOSE 7777/udp
+
+# Copy source code
+COPY --chown=ue4:ue4 .. /tmp/project
+
+ARG SERVER_CONFIG
+ARG PROJECT_FILE_NAME
+ARG UE_BUILD_ARGS
+
+# Clean & build dedicated server for Linux
+RUN /home/ue4/UnrealEngine/Engine/Build/BatchFiles/RunUAT.sh BuildCookRun \
+    -serverconfig=${SERVER_CONFIG} \
+    -project=/tmp/project/${PROJECT_FILE_NAME}.uproject \
+    -utf8output -nodebuginfo -allmaps -noP4 -cook -ddc=NoZenLocalFallback -build -stage -prereqs -pak -package -archive \
+    -archivedirectory=/tmp/project/Packaged \
+    -platform=Linux -server -noclient \
+    ${UE_BUILD_ARGS}
+
+# Second image is used to run the project build
+FROM ubuntu:22.04 AS runtime
+
+ARG TARGET_FILE_NAME=
+ENV TARGET_FILE_NAME=${TARGET_FILE_NAME}
+
+# Specify container command for later (main process)
+CMD ["/home/ue4/project/StartServer.sh"]
+
+# Install runtime dependencies
+RUN apt-get update && \
+    apt-get install openssh-server sudo jq curl -y && \
+    apt-get clean && \
+    rm -rf /var/lib/{apt,dpkg,cache,log}/
+
+ARG BUILDUTILS_FOLDER
+# Copy build and runtime files in the runtime container
+COPY --chown=ue4:ue4 ./${BUILDUTILS_FOLDER}/StartServer.sh /home/ue4/project/StartServer.sh
+RUN sed -i 's/\r$//' /home/ue4/project/StartServer.sh
+COPY --from=builder --chown=ue4:ue4 /tmp/project/Packaged/LinuxServer /home/ue4/project
+
+# Create user
+RUN useradd ue4
+USER ue4
